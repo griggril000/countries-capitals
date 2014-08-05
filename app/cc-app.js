3c218048c4827d548e4c79a1be94d8393a20ebb8
Jordan Hudgens <jordan@wow.com>
2014-08-04 18:24:20 -0700

    integrated routing and styling

---
+++ app/cc-app.js
@@ -0,0 +1,48 @@
+angular.module('CCApp', ['ngRoute', 'ngAnimate'])
+    .value('ccCountries', 
+        ['Peru', 'America', 'Venezuela'])
+	.config(function($routeProvider){
+        $routeProvider.when('/', {
+            templateUrl: './home.html',
+            controller : 'HomeCtrl'
+        })
+        .when('/country/:country', {
+            templateUrl : './country.html',
+            controller : 'CountryCtrl',
+            resolve : {
+                country: function(ccCountries, $route, $location) {
+                    var country = $route.current.params.country;
+                    if(ccCountries.indexOf(country) == -1 ) {
+                        $location.path('/error');
+                        return;
+                    }
+                    return country;
+                }
+            }
+        })
+        .when('/error', {
+		    template : '<p>Error Page Not Found</p>'
+		})
+        .otherwise({
+            redirectTo : '/error'
+        });
+    })
+    .run(function($rootScope, $location, $timeout) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function() {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $timeout(function() {
+                $rootScope.isLoading = false;
+            }, 1000);
+        });
+    })
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CountryCtrl', function($scope, country) {
+   		$scope.country = country;
+	});
